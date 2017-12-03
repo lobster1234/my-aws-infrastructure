9237e32662261188c2dedfc06cfefe53ca78b29e
Manish Pandit <lobster1234@yahoo.com>
2017-12-03 02:34:57 -0800

    Fixed formatting

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,7 +15,7 @@ terraform {
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   tags{
-    Name="My VPC in us-east-1"
+    Name = "My VPC in us-east-1"
   }
 }
 
@@ -25,35 +25,38 @@ resource "aws_subnet" "private-1a" {
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags{
-    Name="Private 1a Subnet"
+    Name = "Private 1a Subnet"
   }
 }
+
 # Another private subnet in us-east-1d
 resource "aws_subnet" "private-1d" {
   vpc_id = "${aws_vpc.myvpc.id}"
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1d"
   tags{
-    Name="Private 1d Subnet"
+    Name = "Private 1d Subnet"
   }
 }
+
 # Public subnet in 1a
 resource "aws_subnet" "public-1a" {
   vpc_id = "${aws_vpc.myvpc.id}"
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   tags{
-    Name="Public 1a Subnet"
+    Name = "Public 1a Subnet"
   }
 }
+
 # Internet Gateway
 resource "aws_internet_gateway" "myigw" {
   vpc_id = "${aws_vpc.myvpc.id}"
-
   tags {
     Name = "MyVPC Inernet Gateway"
   }
 }
+
 # Route Table for the public subnet
 resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.myvpc.id}"
@@ -62,12 +65,12 @@ resource "aws_route_table" "public_route_table" {
     gateway_id = "${aws_internet_gateway.myigw.id}"
   }
   tags{
-    Name="Public Route Table"
+    Name = "Public Route Table"
   }
 }
+
 # Associate this route table with public subnet
 resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = "${aws_subnet.public-1a.id}"
   route_table_id = "${aws_route_table.public_route_table.id}"
 }
-
